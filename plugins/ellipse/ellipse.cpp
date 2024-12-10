af8fcbd8ba6e58ae1a3819b641bb178b0a001d59
Arman <ovakimian.al@phystech.edu>
2024-12-10 20:24:11 +0300

    deleted comment

--- plugins/ellipse/ellipse.cpp
+++ plugins/ellipse/ellipse.cpp
@@ -2,7 +2,7 @@
 
 #include "pluginLib/shapeButtons/shapeButton.hpp"
 
-bool onLoadPlugin() // onLoadPlugin
+bool onLoadPlugin()
 {
     return instrument_button_functions::instrumentButtonOnLoadPlugin<
         ShapeButton<IEllipseShape>, MediatorType>("media/textures/paintbrush.png");
